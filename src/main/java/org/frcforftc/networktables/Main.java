43ff25759c869bb725c0fab7e2558909bd3487e2
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-07-26 10:25:41 +0300

    test-publish

--- src/main/java/org/frcforftc/networktables/Main.java
+++ src/main/java/org/frcforftc/networktables/Main.java
@@ -1,10 +1,10 @@
-package org.frcforftc.networktables;
-
-public class Main {
-    public static void main(String[] args) {
-        NetworkTablesInstance inst = NetworkTablesInstance.getDefaultInstance();
-        inst.startNT4Server();
-        inst.putNumberArray("test1", new double[]{1.0, 2.0, 3.0});
-        inst.putStringArray("test2", new String[]{"t", "b", "c"});
-    }
-}
+//package org.frcforftc.networktables;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        NetworkTablesInstance inst = NetworkTablesInstance.getDefaultInstance();
+//        inst.startNT4Server();
+//        inst.putNumberArray("test1", new double[]{1.0, 2.0, 3.0});
+//        inst.putStringArray("test2", new String[]{"t", "b", "c"});
+//    }
+//}
